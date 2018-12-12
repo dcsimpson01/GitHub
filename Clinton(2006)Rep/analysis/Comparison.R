9e24943f2c14b3498887471f5faa688a1287cdb2
dcsimpson01 <dcsimpson01@gmail.com>
2018-12-12 16:17:30 -0500

    new updates
    updates

--- Clinton(2006)Rep/analysis/Comparison.R
+++ Clinton(2006)Rep/analysis/Comparison.R
@@ -135,9 +135,9 @@ ggplot(data, aes(x=lc,y=x106mean, color=partyname))+
 
 # Theory Plot
 # Same Party vs Independent
-testplot <- ggplot(data = subset(data), aes(x=C_pctsp, y=C_pcti,colour=(x106mean-mean(x106mean))/sd(x106mean))) +
+testplot <- ggplot(data = subset(data,R==1), aes(x=C_pctsp, y=C_pcti/2,colour=x106mean)) + #colour=(x106mean-mean(x106mean))/sd(x106mean))
   geom_point(shape=1,size=2,colour="black") +
-  geom_point(aes(colour=x106mean)) +
+  geom_point()+#aes(colour=x106kmean)) +
   #geom_bin2d()+
   #stat_summary_2d(data,aes(z = splc), bins = 30, fun = mean)+
   geom_rug()+
@@ -160,28 +160,104 @@ ggplot(data, aes(X, Y, z= Z)) + geom_tile(aes(fill = Z)) + theme_bw()
 
 # To change the color of the gradation :
 
-trial <- as.data.frame(matrix(c(rep(0,432*3*8)),nrow=432*3,ncol=8))
+## Beautiful
+data2 <- data[order(-data$R,data$C_pctsp),] #Order data DEM to GOP by percent same party
+switch <- data2[which(data2$R==1),] # select GOP districts
+switch <- switch[order(-switch$C_pctsp),] #order GOP from smalles percent same party to larger
+data2[which(data2$R==1),] <-switch   #Replace the order of GOP
+trial <- as.data.frame(matrix(c(rep(0,432*3*9)),nrow=432*3,ncol=9))
 trial[1:432,2] <- c(rep("SameParty",432))
-trial[1:432,1] <- data[,c("cdcoden")]
-trial[1:432,3:8] <- data[,c("splc","C_pctsp","partyname","lc","x106mean","x106kmean")]
+trial[1:432,1] <- data2[,c("cdcoden")]
+trial[1:432,3:8] <- data2[,c("splc","C_pctsp","partyname","lc","x106mean","x106kmean")]
+trial[1:432,9]  <- c(1:432)
 trial[433:864,2] <- c(rep("Independent",432))
-trial[433:864,1] <- data[,c("cdcoden")]
-trial[433:864,3:8] <- data[,c("ilc","C_pcti","partyname","lc","x106mean","x106kmean")]
+trial[433:864,1] <- data2[,c("cdcoden")]
+trial[433:864,3:8] <- data2[,c("ilc","C_pcti","partyname","lc","x106mean","x106kmean")]
+trial[433:864,9]  <- c(1:432)
 trial[865:1296,2] <- c(rep("OppositeParty",432))
-trial[865:1296,1] <- data[,c("cdcoden")]
-trial[865:1296,3:8] <- data[,c("oplc","C_pctop","partyname","lc","x106mean","x106kmean")]
+trial[865:1296,1] <- data2[,c("cdcoden")]
+trial[865:1296,3:8] <- data2[,c("oplc","C_pctop","partyname","lc","x106mean","x106kmean")]
+trial[865:1296,9]  <- c(1:432)
+
 
 ggplot(data = trial) + geom_bar(aes(y = V4, x = V1, fill = V2),
                     stat="identity")
 
-
-ggplot(data= subset(trial,V5=="Democrat"), aes(x = factor(V1), y = V4, fill = V3)) +
-  geom_bar(position = position_stack(), stat = "identity", width = .7) +
+ 
+q <-ggplot(data= subset(trial), aes(x = factor(V9), y = V4, fill = V3)) +
+  geom_bar(position = position_stack(), stat = "identity", width =.8) +
   #geom_text(aes(label = V2), position = position_stack(vjust = 0.5), size = 2) +
   coord_flip()+
-  scale_fill_gradient2(low = color[1], mid = "white", high = color[2])
+  scale_fill_gradient2(low = color[1], mid = "white", high = color[2])+
+  scale_colour_gradient2(low = color[1], mid = "white", high = color[2])
+
+q <-ggplot(data= subset(trial), aes(x = factor(V9), y = V4, fill = V3)) +
+  geom_bar(position = position_stack(), stat = "identity", width =.8) +
+  #geom_text(aes(label = V2), position = position_stack(vjust = 0.5), size = 2) +
+  coord_flip()+
+  scale_fill_gradient2(low = color[1], mid = "white", high = color[2])+
+  scale_colour_gradient2(low = color[1], mid = "white", high = color[2])
+
+q+ geom_point(data = trial,aes(x =factor(V9), y = (trial$V7+max(trial$V7))/(2*max(trial$V7)), size=1.0)) + 
+  geom_point(data = trial,aes(x =factor(V9), y = (trial$V7+max(trial$V7))/(2*max(trial$V7)), colour=trial$V7))
 
 
+q+ geom_point(data = trial,aes(x =factor(V9), y = (trial$V8+max(trial$V8))/(2*max(trial$V8)), size=1.0)) + 
+  geom_point(data = trial,aes(x =factor(V9), y = (trial$V8+max(trial$V8))/(2*max(trial$V8)), colour=trial$V8))
+
+
+summary(lm((x106kmean-x106mean)~lc+R,data=subset(data)))
+summary(lm((x106kmean-x106mean)~C_pctsp*splc+C_pcti*ilc,data=subset(data)))
+summary(lm((x106kmean-x106mean)~C_pctsp*splc+C_pcti*ilc+C_pctop*oplc,data=subset(data)))
+
+summary(lm((x106kmean-x106mean)~C_pctsp*splc,data=subset(data)))
+summary(lm((x106kmean-x106mean)~C_pctsp*splc+C_pcti*ilc,data=subset(data)))
+summary(lm((x106kmean-x106mean)~C_pctsp*splc+C_pcti*ilc+C_pctop*oplc,data=subset(data)))
+
+summary(lm((x106kmean-x106mean)~C_pctsp*splc,data=subset(data,R==1)))
+summary(lm((x106kmean-x106mean)~C_pctsp*splc+C_pcti*ilc,data=subset(data,R==1)))
+summary(lm((x106kmean-x106mean)~C_pctsp*splc+C_pcti*ilc+C_pctop*oplc,data=subset(data,R==1)))
+
+summary(lm((x106kmean-x106mean)~C_pctsp*splc,data=subset(data,R==0)))
+summary(lm((x106kmean-x106mean)~C_pctsp*splc+C_pcti*ilc,data=subset(data,R==0)))
+summary(lm((x106kmean-x106mean)~C_pctsp*splc+C_pcti*ilc+C_pctop*oplc,data=subset(data,R==0)))
+
+
+# Note do a regression on the change in blah.
+#
+
+ggplot(data= subset(trial), aes(x=pct_2013, xend=pct_2014, y=Area)) + 
+  #create a thick line between x and xend instead of using defaut 
+  #provided by geom_dubbell
+  geom_segment(aes(x=pct_2013, 
+                   xend=pct_2014, 
+                   y=Area, 
+                   yend=Area), 
+               color="#b2b2b2", size=1.5)+
+  geom_dumbbell(color="light blue", 
+                size_x=3.5, 
+                size_xend = 3.5,
+                #Note: there is no US:'color' for UK:'colour' 
+                # in geom_dumbbel unlike standard geoms in ggplot()
+                colour_x="#edae52", 
+                colour_xend = "#9fb059")+
+  labs(x=NULL, y=NULL, 
+       title="Dumbbell Chart", 
+       subtitle="Pct Change: 2013 vs 2014")+
+  geom_text(color="black", size=2, hjust=-0.5,
+            aes(x=pct_2013, label=pct_2013))+
+  geom_text(aes(x=pct_2014, label=pct_2014), 
+            color="black", size=2, hjust=1.5)
+
+
+
+
+
+
+
+
+
+q + aes(x = fct_inorder(V4[which(trial$V2=="SameParty")]))
 
 mean(data$x106kmean[which(data$C_pctsp>.5 & data$R==1)])
 mean(data$x106kmean[which(data$C_pctsp<.5 & data$R==1)])
